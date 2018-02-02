6438536c79ed4163efc6b899aac0b5e9ec82338c
Zhang Tengfei <tengfei.zhang@renren-inc.com>
2018-02-02 10:26:29 +0800

    C

---
+++ test1.c
@@ -0,0 +1,15 @@
+//#include<iostream>
+//using namespace std;
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+	int i=0,sum=0;
+	for(;i<100;++i)
+	{
+		sum+=i;	
+	}
+	printf("%d",sum);
+	return 0;
+
+}
